788d254b1e48d59eb19137d2905c1d62e8243d4a
Shreedhar Hegde <snhkelemane@gmail.com>
2022-02-01 10:08:16 +0530

    changes to interaction

--- src/component/calculator.jsx
+++ src/component/calculator.jsx
@@ -1,23 +1,82 @@
+import { click } from "@testing-library/user-event/dist/click";
 import React, { useState } from "react";
 import Button from "./numberButton";
 import { Body, ColumnBox, Display, HiddenBox } from "./styled";
 
-
 function Calculator() {
-	const [input, setInput] = useState('0');
+	const [input, setInput] = useState("0");
+
+	const addToInput = (value) => {
+		// console.log(typeof (value));
+
+		if (isNaN(value)) {
+			// console.log(value)
+			// setInput((prev) => prev + value);
+			switch (value) {
+				case "+":
+					console.log("addition");
+					break;
+
+				case "-":
+					setInput((prev) => prev + value);
+					console.log("subscription");
+					break;
+
+				case "*":
+					setInput((prev) => prev + value);
+					console.log("multiplication");
+					break;
+
+				case "/":
+					setInput((prev) => prev + value);
+					console.log("division");
+					break;
+
+				case "=":
+					console.log(value);
+					setInput((prev) => prev + value);
+          break;
+        
+				case "AC":
+					setInput("0");
+
+				default:
+					return null;
+			}
+		} else {
+			if (input == "0") {
+				setInput(value);
+			} else {
+				value = value.toString();
+				console.log(value);
+
+				setInput((prev) => prev + value);
+			}
+		}
+	};
+
 	return (
 		<>
 			<Body>
 				<Display>{input}</Display>
 
 				<ColumnBox>
-					<Button values={["AC", "+/-", "%"]} hidden={<HiddenBox />} />
+					<Button
+						buttonClick={addToInput}
+						values={["AC", "+/-", "%"]}
+						hidden={<HiddenBox />}
+					/>
 
-					<Button values={[7, 8, 9, "/"]} />
-					<Button values={[4, 5, 6, "*"]} />
-					<Button values={[1, 2, 3, "-"]} />
+					<Button buttonClick={addToInput} values={[7, 8, 9, "/"]} />
+					<Button buttonClick={addToInput} values={[4, 5, 6, "*"]} />
+					<Button buttonClick={addToInput} values={[1, 2, 3, "-"]} />
 					<HiddenBox></HiddenBox>
-					<Button values={[0, "=", "+"]} left hidden={<HiddenBox />} />
+					<Button
+						buttonClick={addToInput}
+						values={[0, "=", "+"]}
+						left
+						hidden={<HiddenBox />}
+					/>
 				</ColumnBox>
 			</Body>
 		</>
--- src/component/numberButton.jsx
+++ src/component/numberButton.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { RowBox } from "./styled";
 
+function Button({ values, hidden, left = false,buttonClick }) {
 
+ 
 
-
-function Button({ values, hidden, left = false }) {
+  //  for example only values = [7,8,9]
 	return (
 		<RowBox>
 			{left && hidden}
 			{values.map((value, i) => {
-				return <div key={i}>{value}</div>;
+				return (
+					<div
+						key={i}
+						onClick={()=>buttonClick(value)}
+					>
+						{value}
+					</div>
+				);
 			})}
 			{!left && hidden}
 		</RowBox>
